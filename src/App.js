f9c675f957e82585f4e7fc051459c6410ed5e86c
umutplbyk96 <padostudio@hotmail.com>
2021-07-05 22:28:02 +0300

    add media queries to SingleMoviePage component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import "./App.scss";
 import Header from "./containers/Header/Header";
 import Home from "./containers/Home/Home";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
 import SingleMoviePage from "./containers/SingleMoviePage/SingleMoviePage";
 
 const App = () => {
   return (
-    <Router>
-      <Switch>
-        <div className="app">
-          <Route exact path={["/","/page/:number"] }>
+    <div className="app">
+      <Router>
+        <Switch>
+          <Redirect exact from="/" to="/page/1" />
+          <Route exact path={["/", "/page/:number"]}>
             <Header />
             <Home />
           </Route>
-
-
           <Route path="/movie/:page/:genre/:id" component={SingleMoviePage} />
-        </div>
-      </Switch>
-    </Router>
+        </Switch>
+      </Router>
+    </div>
   );
 };
 
--- src/components/Navigations/Navigation.jsx
+++ src/components/Navigations/Navigation.jsx
@@ -1,35 +1,40 @@
 import "./Navigation.scss";
 import request from "../../requests";
-import { useDispatch,useSelector } from "react-redux";
-import { fetchMovies, handleMoviesHeading, handleCurrentPage } from "../../feautures/movies/moviesSlice";
-import {NavLink } from "react-router-dom";
-
+import { useDispatch, useSelector } from "react-redux";
+import {
+  fetchMovies,
+  handleMoviesHeading,
+  handleCurrentPage,
+} from "../../feautures/movies/moviesSlice";
+import { NavLink, useHistory } from "react-router-dom";
 
 const Navigation = () => {
+  const history = useHistory()
   const dispatch = useDispatch();
-  const moviesCurrentPage = useSelector(state => state.movies.currentPage)
+  const moviesCurrentPage = useSelector((state) => state.movies.currentPage);
 
   const handleLinks = (link) => {
     dispatch(fetchMovies(link));
   };
 
   const handleHeading = (heading) => {
-    dispatch(handleMoviesHeading(heading))
-  }
-
-
+    dispatch(handleMoviesHeading(heading));
+  };
 
   return (
     <div className="nav">
       <ul className="nav__list">
         <li className="nav__item">
           <NavLink
-            to={`/page/${moviesCurrentPage}`}
+            to=""
             className="nav__link"
             onClick={() => {
-              dispatch(handleCurrentPage(1))
+              console.log("burası çalıştı abi");
+
+              dispatch(handleCurrentPage(1));
               handleLinks(request.fetchPopular());
-              handleHeading("POPULAR")
+              handleHeading("POPULAR");
+              history.push(`/page/${moviesCurrentPage}`)
             }}
           >
             Popular
@@ -37,12 +42,16 @@ const Navigation = () => {
         </li>
         <li className="nav__item">
           <NavLink
-            to={`/page/${moviesCurrentPage}`}
+            to=""
             className="nav__link"
             onClick={() => {
-              dispatch(handleCurrentPage(1))
+              console.log("burası çalıştı abi");
+
+              dispatch(handleCurrentPage(1));
               handleLinks(request.fetchUpComing());
-              handleHeading("UP COMING")
+              handleHeading("UP COMING");
+              history.push(`/page/${moviesCurrentPage}`)
+              console.log("şuan ki sayfa : ", moviesCurrentPage);
             }}
           >
             Upcoming
@@ -50,12 +59,16 @@ const Navigation = () => {
         </li>
         <li className="nav__item">
           <NavLink
-            to={`/page/${moviesCurrentPage}`}
+            to=""
             className="nav__link"
             onClick={() => {
-              dispatch(handleCurrentPage(1))
+              console.log("burası çalıştı abi");
+
+              dispatch(handleCurrentPage(1));
               handleLinks(request.fetchNowPlaying());
-              handleHeading("NOW PLAYING")
+              handleHeading("NOW PLAYING");
+              history.push(`/page/${moviesCurrentPage}`);
+              console.log("şuan ki sayfa : ", moviesCurrentPage);
             }}
           >
             Now Playing
